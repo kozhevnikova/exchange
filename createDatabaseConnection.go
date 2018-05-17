66d71f278dbefa15087d2d3e0b13468266a4233a
Jane <jane.kozhevnikova@gmail.com>
2018-05-17 19:59:00 +0300

    Database connection was added

---
+++ createDatabaseConnection.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func createDatabaseConnection(config Config) (*sql.DB, error) {
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable",
+		config.Database.User, config.Database.Password, config.Database.Name, config.Database.Host)
+	db, err := sql.Open("postgres", dbinfo)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
